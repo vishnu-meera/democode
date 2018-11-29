4b6fc84897c1ae5f60a98e991530a5e5443fd594
Vishnu Sankar <vishgitt@gmail.com>
2018-11-29 11:05:54 -0800

    Countries Status API

---
+++ Core/QueryHandler/GetStatusCountQueryHandler.cs
@@ -0,0 +1,53 @@
+﻿// Copyright(c) Microsoft Corporation. 
+// All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the solution root folder for full license information
+
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Models;
+using Core.Interfaces;
+using Core.Queries;
+using Core.Exceptions;
+using System.Collections.Generic;
+
+namespace Core.QueryHandler
+{
+    public class GetStatusCountQueryHandler : MediatR.IRequestHandler<GetStatusCount, StatusList>
+    {
+        private readonly IRepository _repository;
+        private readonly string CountryPartionKey = "Countries"; //get it from appsettings
+
+        public GetStatusCountQueryHandler(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<StatusList> Handle(GetStatusCount request, CancellationToken cancellationToken)
+        {
+            var statusList = new StatusList();
+
+            statusList.CountriesStatusList = new Dictionary<string, int>();
+
+            var items = await _repository.GetAll<Country>(nameof(Country), CountryPartionKey);
+            foreach (var item in items)
+            {
+                statusList.CountriesStatusList.TryGetValue(item.Status, out var currentCount);
+                statusList.CountriesStatusList[item.Status] = currentCount + 1;
+            }
+
+            if (items == null)
+            {
+                throw new NotFoundException(nameof(Country), "Countries Overall Status Check");
+            }
+
+            if (items.Count() == 0)
+            {
+                throw new NotFoundException(nameof(Country), "Countries Overall Status Check");
+            }
+
+            return statusList;
+        }
+    }
+}
---
+++ WebApp/Api/CountriesStatusController.cs
@@ -0,0 +1,28 @@
+﻿// Copyright(c) Microsoft Corporation. 
+// All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the solution root folder for full license information
+
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Core.Models;
+using Core.Queries;
+
+namespace WebApp.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesStatusController : BaseController
+    {
+
+        //GET : api/CountriesStatus ==> Overall Status Counts
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<StatusList>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Get([FromQuery] GetStatusCount query)
+        {
+            return Ok(await Mediator.Send(query));
+        }
+
+    }
+}
