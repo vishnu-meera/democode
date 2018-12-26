fe69eeddbca0e5ef5643c869c35f44e2f8a092a7
Vishnu Sankar <vishgitt@gmail.com>
2018-12-26 15:11:15 -0800

    auth bug fix

--- WebApp/ClientApp/src/views/dashboard.jsx
+++ WebApp/ClientApp/src/views/dashboard.jsx
@@ -40,8 +40,8 @@ class Dashboard extends React.Component {
     }
 
     async componentDidMount() {
-        // let {authenticated,token} = await this.auth.isAuthenticated();
-        // if(authenticated){
+        let {authenticated,token} = await this.auth.isAuthenticated();
+        if(authenticated){
             console.log("dashboard===> authenticated");
             if (this.state.loading) {
                 //getting data for card tiles
@@ -52,10 +52,10 @@ class Dashboard extends React.Component {
                 let tableFeedData = this.utils.cloneObject(tableData);
                 this.setState({ mapFeedData,tableFeedData,cardsStatus, mapData,tableData,loading:false,toolTipObject,CountriesObject})
             }
-        // }else{
-        //     console.log("dashboard===> not authenticated");
-        //     this.props.history.push("/admin");
-        // }
+        }else{
+            console.log("dashboard===> not authenticated");
+            this.props.history.push("/admin");
+        }
     }
 
     handleClick =(event,code,tip)=>{
